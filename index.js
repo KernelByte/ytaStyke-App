d271df8793577cdf79e1eb18a2cda177ff6a8bf4
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-03-06 21:28:54 -0500

    Creacion del servidor node

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Ny app");
+// Nos traemos a express
+const express = require("express");
+
+// Ejecutamod el metodo de express
+const app = express();
+
+// Añadimos el puerto por el que escuchara la app
+const port = 3000;
+
+//iniciamos la app
+app.get("/",(require,response)=>{
+  response.send("Inicio del server en Express.");
+});
+
+// Se le indica el puerto a la app
+app.listen(port,()=>{
+  console.log("Mi puerto es: "+port)
+})

2dd0b78f57d53dbb8df570b826b4361364f73ec0
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-03-07 23:52:13 -0500

    Se añaden los demas archivos para cada ruta de los elementos

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
+// Nos traemos a express
+const express = require("express");
+
 const productsRouter = require("./productsRouter");
+const usersRouter = require("./usersRouter");
+const rolesRouter = require("./rolesRouter");
+const buysRouter = require("./buysRouter");
+const statusRouter = require("./statusRouter");
+const groupsRouter = require("./groupsRouter");
 
 function routerApi(app){
-  app.use("/products",productsRouter);
+  // Ruta maestra
+  const router = express.Router();
+  app.use("/api/v1",router)
+
+  router.use("/products",productsRouter);
+  router.use("/users",usersRouter);
+  router.use("/roles",rolesRouter);
+  router.use("/buys",buysRouter);
+  router.use("/status",statusRouter)
+  router.use("/groups",groupsRouter)
 }
+
+module.exports = routerApi;

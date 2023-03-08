67985b4dd2696e84d26aeee9b6260b04ae51258f
kernelbyte <kernelbyte@KernelByte.kerneldomain>
2023-03-08 13:57:05 -0500

    Se realiza prueba de rutas usando el postman en kali linux

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@
 const express = require("express");
 
 const productsRouter = require("./productsRouter");
-const usersRouter = require("./usersRouter");
-const rolesRouter = require("./rolesRouter");
-const buysRouter = require("./buysRouter");
-const statusRouter = require("./statusRouter");
-const groupsRouter = require("./groupsRouter");
+//const usersRouter = require("./usersRouter");
+//const rolesRouter = require("./rolesRouter");
+//const buysRouter = require("./buysRouter");
+//const statusRouter = require("./statusRouter");
+//const groupsRouter = require("./groupsRouter");
 
 function routerApi(app){
   // Ruta maestra
@@ -14,11 +14,11 @@ function routerApi(app){
   app.use("/api/v1",router)
 
   router.use("/products",productsRouter);
-  router.use("/users",usersRouter);
-  router.use("/roles",rolesRouter);
-  router.use("/buys",buysRouter);
-  router.use("/status",statusRouter)
-  router.use("/groups",groupsRouter)
+  //router.use("/users",usersRouter);
+  //router.use("/roles",rolesRouter);
+  //router.use("/buys",buysRouter);
+  //router.use("/status",statusRouter)
+  //router.use("/groups",groupsRouter)
 }
 
 module.exports = routerApi;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,12 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 // listar products
-app.router("/", (request, response) => {
+router.get("/", (request, response) => {
   response.json([{name : "yoniher", age : 18}, {name : "Oscar", age : 23}]);
 });
 
 // Consultar un product
-app.router("/:id", (request, response) => {
+router.get("/:id", (request, response) => {
   const {id} = request.params;
   response.json({id, name : "yoniher", age : 18});
 });

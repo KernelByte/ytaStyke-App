df749c25e5e85baf6235eb7850b0dcf279f06657
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-03-07 22:55:40 -0500

    Se añade url de consulta productos

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ const app = express();
 const port = 3000;
 
 //iniciamos la app en la siguiente ruta
-app.get("/", (require, response) => {
+app.get("/", (request, response) => {
   response.send("Inicio del server en Express.");
+});
+
+// listar products
+app.get("/list_products", (request, response) => {
+  response.json([{name : "yoniher", age : 18}, {name : "Oscar", age : 23}]);
+});
+
+
+// Consultar un product
+app.get("/products/:id", (request, response) => {
+  const {id} = request.params;
+  response.json({id, name : "yoniher", age : 18});
 });
 
 // Se le indica el puerto a la app

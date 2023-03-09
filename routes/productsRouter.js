40aa557b4ae053c0e6418a1993b279c8e4711cd9
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-09 12:32:02 -0500

    Se añade el uso de los status code en las respuestas HTTP

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,32 +4,38 @@ const router = express.Router();
 
 // listar products
 router.get("/", (request, response) => {
-  response.json([{name : "yoniher", age : 18}, {name : "Oscar", age : 23}]);
+  response.json([{ name: "yoniher", age: 18 }, { name: "Oscar", age: 23 }]);
 });
 
 // Consultar un product
 router.get("/:id", (request, response) => {
-  const {id} = request.params;
-  response.json({id, name : "yoniher", age : 18});
+  const { id } = request.params;
+  if (id === "999") {
+    response.status(404).json({
+      message: "Not Found"
+    })
+  } else {
+    response.status(200).json({ id, name: "yoniher", age: 18 });
+  }
 });
 
 //Crear un nuevo producto
 router.post("/", (request, response) => {
   const bodyResponse = request.body;
-  response.json({
-    message : "User create",
-    data : bodyResponse
+  response.status(201).json({
+    message: "User create",
+    data: bodyResponse
   });
 });
 
 //Actualizar un dato del producto
 router.patch("/:id", (request, response) => {
   // se recibe id
-  const {id} = request.params;
+  const { id } = request.params;
   const bodyResponse = request.body;
   response.json({
-    message : "Product update",
-    data : bodyResponse,
+    message: "Product update",
+    data: bodyResponse,
     id,
   });
 });
@@ -37,9 +43,9 @@ router.patch("/:id", (request, response) => {
 //Eliminar un producto
 router.delete("/:id", (request, response) => {
   // se recibe id
-  const {id} = request.params;
+  const { id } = request.params;
   response.json({
-    message : "Product delete",
+    message: "Product delete",
     id,
   });
 });

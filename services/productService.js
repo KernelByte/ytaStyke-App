7f0671c419ce512b98a77c14714481f4e354457f
kernelbyte <kernelbyte@KernelByte.kerneldomain>
2023-03-09 20:02:21 -0500

    Se formatea el codigo del servicio productos

--- services/productService.js
+++ services/productService.js
@@ -1,67 +1,69 @@
 class productService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
 
-   constructor() {
-      this.products = [];
-      this.generate();
-   };
-
-   // Create a product
-   create(pro) {
+  // Create a product
+  create(pro) {
     this.products.push(pro);
-   };
+  }
 
-   // Find all products
-   find() {
-      return this.products;
-   };
+  // Find all products
+  find() {
+    return this.products;
+  }
 
-   // Find one product
-   findOne(id) {
-      return this.products.find(product => product.id === id);
-   };
+  // Find one product
+  findOne(id) {
+    return this.products.find((product) => product.id === id);
+  }
 
-   // Update a product
-   update(id, changes) {
-    const index = this.products.findIndex(product => product.id === id);
-    if (index === -1){
-      throw new Error("product not found");
-    }else{
+  // Update a product
+  update(id, changes) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new Error('product not found');
+    } else {
       const pro = this.products[index];
       this.products[index] = {
         ...pro,
-        ...changes
-      }
-      return {message : true};
+        ...changes,
+      };
+      return { message: true };
     }
-   };
+  }
 
-   // Delete a product
-   delete(id) {
-    const index = this.products.findIndex(product => product.id === id);
-    if (index === -1){
-      throw new Error("product not found");
-    }else{
-      this.products.splice(index,1);
-      return {message : true};
+  // Delete a product
+  delete(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new Error('product not found');
+    } else {
+      this.products.splice(index, 1);
+      return { message: true };
     }
-   };
+  }
 
-   generate() {
-      this.products.push({
-         "id": "10",
-         "name": "yoniher",
-         "age": 18
+  generate() {
+    this.products.push(
+      {
+        id: '10',
+        name: 'yoniher',
+        age: 18,
       },
-         {
-            "id": "20",
-            "name": "Oscar",
-            "age": 19
-         }, {
-         "id": "30",
-         "name": "Luis",
-         "age": 20
-      })
-   }
+      {
+        id: '20',
+        name: 'Oscar',
+        age: 19,
+      },
+      {
+        id: '30',
+        name: 'Luis',
+        age: 20,
+      }
+    );
+  }
 }
 
 module.exports = productService;

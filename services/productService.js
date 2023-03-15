a8bef0f216c085e6e314760777e6fdcc1a2446ef
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-15 12:01:38 -0500

    Se realiza consulta a la base de datos usando la programacion orientada a objetos con modelo de sequelize

--- services/productService.js
+++ services/productService.js
@@ -1,5 +1,4 @@
 // Conexion directa a sequelize
-//const sequelize = require('../libs/sequelize');
 const {models} = require('../libs/sequelize');
 
 class productService {
@@ -15,9 +14,8 @@ class productService {
 
   // Find all products
   async find() {
-    const query = 'SELECT * FROM public.products';
-    const [data, metadata] = await sequelize.query(query);
-    return { data };
+    const query = await models.Product.findAll();
+    return query;
   }
 
   // Find one product

ae7a0e46a34ebccdbf607d5081874b6ed2795f03
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-22 12:48:48 -0500

    Se añade modelo de base de datos para la tabla Users

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,4 +1,58 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
 
 // Definimos la tabla
-const PRODUCT_TABLE = 'users';
+const USERS_TABLE = 'users';
+
+// Definimos el esquema de la base de datos
+const UserSchema = {
+  id_user: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+
+  number_id: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+
+  name_user: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  password_user: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  id_role_user: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+};
+
+class User extends Model {
+  //Metodo para declarar todas las relaciones
+  static associate() {
+    //associate
+  }
+
+  //Metodo para configuracion
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: USERS_TABLE,
+      modelName: 'user',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { USERS_TABLE, UserSchema, User };

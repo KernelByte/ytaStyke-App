df1ba5d4738d8dff917e7b07d69eafed358d1ae2
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-22 12:45:03 -0500

    Se añade modelo de base de datos para la tabla status

--- db/models/statuModel.js
+++ db/models/statuModel.js
@@ -1,4 +1,43 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
 
 // Definimos la tabla
-const PRODUCT_TABLE = 'status';
+const STATUS_TABLE = 'status';
+
+// Definimos el esquema de la base de datos
+const StatuSchema = {
+  id_status: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+
+  group_status: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  description: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+};
+
+class Statu extends Model {
+  //Metodo para declarar todas las relaciones
+  static associate() {
+    //associate
+  }
+
+  //Metodo para configuracion
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: STATUS_TABLE,
+      modelName: 'Statu',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { STATUS_TABLE, StatuSchema, Statu };

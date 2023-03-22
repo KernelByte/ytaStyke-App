9a8153965366c40d1fc0755af9c40ef29961fb31
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-22 12:55:27 -0500

    Se añaden los modelos al archivo index de inicalizacion sequelize

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,19 @@
 const {Product, ProductSchema} = require("./productModel");
-//const {Product, ProductSchema} = require("./productModel"); -- Otro modelo
+const {Buy, buySchema} = require("./buyModel");
+const {Group, GroupSchema} = require("./groupModel");
+const {PriceProduct, PriceProductSchema} = require("./priceProductModel");
+const {Role, RoleSchema} = require("./roleModel");
+const {Statu, StatuSchema} = require("./statuModel");
+const {User, UserSchema} = require("./userModel");
 
 function setupModels(sequelize){
   Product.init(ProductSchema,Product.config(sequelize));
-  //Product.init(ProductSchema,Product.config(sequelize)); -- Otro modelo
+  Buy.init(buySchema,Buy.config(sequelize));
+  Group.init(GroupSchema,Group.config(sequelize));
+  PriceProduct.init(PriceProductSchema,PriceProduct.config(sequelize));
+  Role.init(RoleSchema,Role.config(sequelize));
+  Statu.init(StatuSchema,Statu.config(sequelize));
+  User.init(UserSchema,User.config(sequelize));
 };
 
 module.exports = setupModels;

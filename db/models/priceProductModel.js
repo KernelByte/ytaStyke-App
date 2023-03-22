b3953f146357bd2bb052c2eaaf80dae1023f976f
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-22 12:42:27 -0500

    Se añade modelo de base de datos para la tabla price_products

--- db/models/priceProductModel.js
+++ db/models/priceProductModel.js
@@ -1,4 +1,53 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
 
 // Definimos la tabla
-const PRODUCT_TABLE = 'price_products';
+const PRICE_PRODUCTS_TABLE = 'price_products';
+
+// Definimos el esquema de la base de datos
+const PriceProductSchema = {
+  id_price_product: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+
+  id_group_price_product: {
+    type: DataTypes.INTEGER,
+  },
+
+  name_product: {
+    type: DataTypes.STRING,
+  },
+
+  price_unit: {
+    type: DataTypes.INTEGER,
+  },
+
+  price_minimum: {
+    type: DataTypes.INTEGER,
+  },
+
+  price_sending: {
+    type: DataTypes.INTEGER,
+  },
+};
+
+class PriceProduct extends Model {
+  //Metodo para declarar todas las relaciones
+  static associate() {
+    //associate
+  }
+
+  //Metodo para configuracion
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRICE_PRODUCTS_TABLE,
+      modelName: 'PriceProduct',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { PRICE_PRODUCTS_TABLE, PriceProductSchema, PriceProduct };

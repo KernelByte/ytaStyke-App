68bae101461305190def960c1a921be86030736e
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-03-23 01:04:47 -0500

    Se añade migracion con todas las tablas a crear

---
+++ db/migrations/20230323052508-Create-data-base.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { BuySchema, BUYS_TABLE } = require('./../models/buyModel');
+const { GroupSchema, GROUPS_TABLE } = require('./../models/groupModel');
+const { PriceProductSchema, PRICE_PRODUCTS_TABLE } = require('./../models/priceProductModel');
+const { ProductSchema, PRODUCTS_TABLE } = require('./../models/productModel');
+const { RoleSchema, ROLES_TABLE } = require('./../models/roleModel');
+const { StatuSchema, STATUS_TABLE } = require('./../models/statuModel');
+const { UserSchema, USERS_TABLE } = require('./../models/userModel');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(BUYS_TABLE, BuySchema);
+    await queryInterface.createTable(GROUPS_TABLE, GroupSchema);
+    await queryInterface.createTable(PRICE_PRODUCTS_TABLE, PriceProductSchema);
+    await queryInterface.createTable(PRODUCTS_TABLE, ProductSchema);
+    await queryInterface.createTable(ROLES_TABLE, RoleSchema);
+    await queryInterface.createTable(STATUS_TABLE, StatuSchema);
+    await queryInterface.createTable(USERS_TABLE, UserSchema);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(BUYS_TABLE);
+    await queryInterface.dropTable(GROUPS_TABLE);
+    await queryInterface.dropTable(PRICE_PRODUCTS_TABLE);
+    await queryInterface.dropTable(PRODUCTS_TABLE);
+    await queryInterface.dropTable(ROLES_TABLE);
+    await queryInterface.dropTable(STATUS_TABLE);
+    await queryInterface.dropTable(USERS_TABLE);
+  },
+};

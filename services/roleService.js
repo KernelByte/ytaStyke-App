f965472bda2c35c2a95150a605362575f0d4a8a7
Yoniher Melendez <yonihermelendez@gmail.com>
2023-03-25 23:45:09 -0500

    Se realiza creacion de CRUD para el service de roles

--- services/roleService.js
+++ services/roleService.js
@@ -10,11 +10,36 @@ class roleService {
     return { message: 'Rol Creado' };
   }
 
-    // Find all roles
-    async find() {
-      const queryAll = await models.Role.findAll();
-      return queryAll;
+  // Find all roles
+  async find() {
+    const queryAll = await models.Role.findAll();
+    return queryAll;
+  }
+
+  // Find one role
+  async findOne(id) {
+    const findRole = await models.Role.findByPk(id);
+    if (!findRole) {
+      throw new Error('product not found');
+    } else {
+      return findRole;
     }
+  }
+
+  // Update a role
+  async update(id, changes) {
+    // Busqueda del rol a actualizar
+    const findRole = await this.findOne(id);
+    await findRole.update(changes);
+  }
+
+  // Delete a role
+  async delete(id) {
+    // Busqueda del producto a actualizar
+    const findRole = await this.findOne(id);
+    await findRole.destroy();
+    return { message: true };
+  }
 }
 
 module.exports = roleService;
